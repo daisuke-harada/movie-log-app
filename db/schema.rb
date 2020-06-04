c97aeb409f84adb83aaa698419a1e8da85d0d21a
daisuke <daisukeh9404031099@gmail.com>
2020-06-04 01:54:54 +0000

    [add] writerモデルを追加、migrate完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_015041) do
+ActiveRecord::Schema.define(version: 2020_06_04_015321) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -107,6 +107,14 @@ ActiveRecord::Schema.define(version: 2020_06_04_015041) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "writers", force: :cascade do |t|
+    t.integer "movie_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_writers_on_movie_id"
   end
 
 end
