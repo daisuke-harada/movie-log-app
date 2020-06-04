843a80740043bc4d8c9ed02b521d9011b8e34c6f
daisuke <daisukeh9404031099@gmail.com>
2020-06-04 01:58:47 +0000

    [add] actorモデルを追加、migrate完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_015321) do
+ActiveRecord::Schema.define(version: 2020_06_04_015739) do
+
+  create_table "actors", force: :cascade do |t|
+    t.integer "movie_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_actors_on_movie_id"
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
