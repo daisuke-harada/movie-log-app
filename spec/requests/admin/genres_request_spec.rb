ffb6d141978b98c444f582addb72f75a5a9fdfde
daisuke <daisukeh9404031099@gmail.com>
2020-06-05 01:38:44 +0000

    [add] genre indexのコントローラとviewにテストを追加。

--- spec/requests/admin/genres_request_spec.rb
+++ spec/requests/admin/genres_request_spec.rb
@@ -1,17 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Genres", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admin/genres/index"
-      expect(response).to have_http_status(:success)
+  describe "管理者/ジャンル一覧ページ" do
+    context "管理者/ジャンル一覧ページが正しく表示される。" do
+      before do
+        get admin_genres_path
+      end
+      it "リクエストは200 OKとなること" do
+        expect(response.status).to eq 200
+      end
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("ジャンル一覧")
+      end
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/admin/genres/edit"
-      expect(response).to have_http_status(:success)
+  describe "管理者/ジャンル編集ページ" do
+    context "管理者/ジャンル編集ページが正しく表示される。" do
+      before do
+        get edit_admin_genre_path
+      end
+      it "リクエストは200 OKとなること" do
+        expect(response.status).to eq 200
+      end
     end
   end
 end
