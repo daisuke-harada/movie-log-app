b93fa700d541e3e6a1c2db71793ce3da003fec58
daisuke <daisukeh9404031099@gmail.com>
2020-06-12 09:36:16 +0000

    [add] 映画データをseedで追加。ジャンルの追加方法のみわからない

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,6 @@
   User.create!( name: 'koyo', email: 'koyo_h940403@icloud.com', password: '04030403')
   User.create!( name: 'tom', email: 'tom_h940403@icloud.com', password: '04030403')
 
-  Movie.create!(
-    title: 'アベンジャーズ エンドゲーム',
-    summary: '最強を超える敵“サノス”によって、アベンジャーズのメンバーを含む全宇宙の生命の半分が一瞬で滅ぼされてしまった…。残されたアイアンマンをはじめとするヒーローたちはもう一度集結し、サノスに立ち向かうため、そして世界を救うために最後にして史上最大の戦いに挑む──。',
-    movie_image_id: 'avengers_endgame.jpg',
-    a_movie_released: '2019年4月26日',
-    show_time: '181',
-    )
-
-
   Genre.create!( name: 'アクション')
   Genre.create!( name: 'ドラマ')
   Genre.create!( name: '恋愛')
@@ -41,3 +32,31 @@
   Genre.create!( name: 'パニック')
   Genre.create!( name: '歴史')
   Genre.create!( name: 'ギャング・マフィア')
+
+  Movie.create!(
+    title: 'アベンジャーズ エンドゲーム',
+    summary: '最強を超える敵“サノス”によって、アベンジャーズのメンバーを含む全宇宙の生命の半分が一瞬で滅ぼされてしまった…。残されたアイアンマンをはじめとするヒーローたちはもう一度集結し、サノスに立ち向かうため、そして世界を救うために最後にして史上最大の戦いに挑む──。',
+    movie_image: File.open('app/assets/images/avengers_endgame.jpg'),
+    a_movie_released: '2019年4月26日',
+    show_time: '181分',
+    supervisers_attributes: [
+      {superviser_name: 'アンソニー・ルッソ'},
+      {superviser_name: 'ジョー・ルッソ'}
+    ],
+    actors_attributes: [
+      {actor_name: 'ロバート・ダウニーJr'},
+      {actor_name: 'クリス・エヴァンズ'},
+      {actor_name: 'マーク・ラファロ'},
+      {actor_name: 'クリス・ヘムズワース'},
+      {actor_name: 'スカーレット・ヨハンソン'},
+      {actor_name: 'ジェレミー・レナー'},
+      {actor_name: 'ドン・チードル'},
+      {actor_name: 'ポール・ラッド'},
+      {actor_name: 'ベネディクト・カーンバッチ'},
+      {actor_name: 'チャドウィック・ボーズマン'}
+    ],
+    writers_attributes: [
+      {writer_name: 'クリストファー・マルクス'},
+      {writer_name: 'スティーヴン・マクフィーリー'}
+    ]
+  )
