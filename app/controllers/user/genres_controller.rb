0b352b052dc4f61cf615e1d335a0988c7a4b51d6
daisuke <daisukeh9404031099@gmail.com>
2020-06-19 06:57:04 +0000

    [add] userページのgenre一覧機能、映画表示機能が完成

--- app/controllers/user/genres_controller.rb
+++ app/controllers/user/genres_controller.rb
@@ -1,7 +1,11 @@
 class User::GenresController < ApplicationController
   def index
+    @genres = Genre.all
   end
 
   def show
+    @genres = Genre.all
+    @genre = Genre.find(params[:id])
+    @movies = @genre.movies
   end
 end
