b505386a711cd630856cb1227ad00c91dbca8b0c
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-07-12 02:16:25 +0900

    [fix] asset.rb application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Rails.application.config.assets.paths << Rails.root.join("vendor", "original_assets", "images")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "original_assets", "stylesheets")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "original_assets", "javascripts")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "javascripts")
 
 module MovieLogApp
   class Application < Rails::Application
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,11 +6,11 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-# Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.paths << Rails.root.join("vendor", "original_assets", "images")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "original_assets", "stylesheets")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "original_assets", "javascripts")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "javascripts")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
