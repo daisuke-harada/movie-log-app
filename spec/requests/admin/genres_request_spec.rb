46aa4ca4e1a98b5f150c4a4cc1e216a3543c5a55
daisuke <daisukeh9404031099@gmail.com>
2020-07-15 13:43:43 +0000

    [fix] rspec admin genreとmovie

--- spec/requests/admin/genres_request_spec.rb
+++ spec/requests/admin/genres_request_spec.rb
@@ -12,23 +12,24 @@ RSpec.describe "Admin::Genres", type: :request do
       end
 
       it 'タイトルが正しく表示されていること' do
-        expect(response.body).to include("ジャンル一覧")
+        expect(response.body).to include("ジャンル")
       end
 
       it 'ジャンル新規登録が表示されていること' do
         expect(response.body).to include("ジャンル新規登録")
       end
+
+      it
     end
   end
 
   describe "管理者/ジャンル編集ページ" do
-    before do
-      @genre = { id: 1, name: "ラブロマンス" }
-    end
-
     context "管理者/ジャンル編集ページが正しく表示される。" do
+      let(:params) { {id: 1, name: 'test'} }
+
       before do
-        get edit_admin_genre_path(@genre.id)
+        @genre = Genre.create(params)
+        get edit_admin_genre_path(@genre)
       end
 
       it "リクエストは200 OKとなること" do
