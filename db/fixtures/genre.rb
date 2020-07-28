3518694fa16c5e76f0956ebd1073370afe7d757b
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-07-28 18:44:17 +0900

    [add] seed_fu movie user genre

---
+++ db/fixtures/genre.rb
@@ -0,0 +1,67 @@
+Genre.seed do |s|
+  s.name = "アクション"
+end
+
+Genre.seed do |s|
+  s.name = "ドラマ"
+end
+
+Genre.seed do |s|
+  s.name = "恋愛"
+end
+
+Genre.seed do |s|
+  s.name = "アニメ"
+end
+
+Genre.seed do |s|
+  s.name = "ミステリー"
+end
+
+Genre.seed do |s|
+  s.name = "ミュージカル"
+end
+
+Genre.seed do |s|
+  s.name = "コメディ"
+end
+
+Genre.seed do |s|
+  s.name = "戦争"
+end
+
+Genre.seed do |s|
+  s.name = "青春"
+end
+
+Genre.seed do |s|
+  s.name = "クライム"
+end
+
+Genre.seed do |s|
+  s.name = "スリラー"
+end
+
+Genre.seed do |s|
+  s.name = "サスペンス"
+end
+
+Genre.seed do |s|
+  s.name = "SF"
+end
+
+Genre.seed do |s|
+  s.name = "スポーツ"
+end
+
+Genre.seed do |s|
+  s.name = "ドキュメンタリー"
+end
+
+Genre.seed do |s|
+  s.name = "ファンタジー"
+end
+
+Genre.seed do |s|
+  s.name = "ギャング・マフィア"
+end
---
+++ db/fixtures/relationship.rb
@@ -0,0 +1,129 @@
+Relationship.seed do |s|
+  s.follower_id = 2
+  s.following_id =3
+end
+
+Relationship.seed do |s|
+  s.follower_id = 2
+  s.following_id = 5
+end
+
+Relationship.seed do |s|
+  s.follower_id = 2
+  s.following_id = 8
+end
+
+Relationship.seed do |s|
+  s.follower_id = 2
+  s.following_id = 12
+end
+
+Relationship.seed do |s|
+  s.follower_id = 3
+  s.following_id = 4
+end
+
+Relationship.seed do |s|
+  s.follower_id = 3
+  s.following_id = 6
+end
+
+Relationship.seed do |s|
+  s.follower_id = 3
+  s.following_id = 9
+end
+
+Relationship.seed do |s|
+  s.follower_id = 4
+  s.following_id = 5
+end
+
+Relationship.seed do |s|
+  s.follower_id = 5
+  s.following_id = 6
+end
+
+Relationship.seed do |s|
+  s.follower_id = 5
+  s.following_id = 8
+end
+
+Relationship.seed do |s|
+  s.follower_id = 5
+  s.following_id = 11
+end
+
+Relationship.seed do |s|
+  s.follower_id = 6
+  s.following_id = 11
+end
+
+Relationship.seed do |s|
+  s.follower_id = 7
+  s.following_id = 8
+end
+
+Relationship.seed do |s|
+  s.follower_id = 7
+  s.following_id = 10
+end
+
+Relationship.seed do |s|
+  s.follower_id = 8
+  s.following_id = 10
+end
+
+Relationship.seed do |s|
+  s.follower_id = 9
+  s.following_id = 13
+end
+
+Relationship.seed do |s|
+  s.follower_id = 9
+  s.following_id = 12
+end
+
+Relationship.seed do |s|
+  s.follower_id = 10
+  s.following_id = 11
+end
+
+Relationship.seed do |s|
+  s.follower_id = 11
+  s.following_id = 1
+end
+
+Relationship.seed do |s|
+  s.follower_id = 12
+  s.following_id = 13
+end
+
+Relationship.seed do |s|
+  s.follower_id = 12
+  s.following_id = 14
+end
+
+Relationship.seed do |s|
+  s.follower_id = 12
+  s.following_id = 2
+end
+
+Relationship.seed do |s|
+  s.follower_id = 13
+  s.following_id = 4
+end
+
+Relationship.seed do |s|
+  s.follower_id = 14
+  s.following_id = 2
+end
+
+Relationship.seed do |s|
+  s.follower_id = 14
+  s.following_id = 3
+end
+
+Relationship.seed do |s|
+  s.follower_id = 14
+  s.following_id = 5
+end
