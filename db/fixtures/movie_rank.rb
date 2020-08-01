134e565c54cab6f8e62c56c364d036ab6ad68158
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-08-01 21:50:49 +0900

    [add] seed_fu

---
+++ db/fixtures/movie_rank.rb
@@ -0,0 +1,17 @@
+MovieRank.seed do |s|
+  s.review_id = 1
+  s.user_id = 2
+  s.rank_status = "2位"
+end
+
+MovieRank.seed do |s|
+  s.review_id = 2
+  s.user_id = 2
+  s.rank_status = "1位"
+end
+
+MovieRank.seed do |s|
+  s.review_id = 3
+  s.user_id = 2
+  s.rank_status = "3位"
+end
--- db/fixtures/review.rb
+++ db/fixtures/review.rb
@@ -13,3 +13,11 @@ Review.seed do |s|
   s.content = "今まで見た映画の中で一番好きです。\r\n生きる希望がもらえますし、元気が出ます。何かで悩んでいる方にぜひ見て欲しいです。"
   s.is_spoil = false
 end
+
+Review.seed do |s|
+  s.movie_id = 3
+  s.user_id = 2
+  s.rate = 4.5
+  s.content = "私にとって恋愛映画の中では一番感動した映画です。\r\nこれから生きていく上で絶対に見た方がいいとお勧めできます。"
+  s.is_spoil = false
+end
