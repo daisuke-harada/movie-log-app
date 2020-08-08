9828edb11956db90befa4fbc1332bddb8f03951d
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-08-08 23:51:56 +0900

    [fix] rspecを変更 factoriesにmovie_genreを追加 全体のインデントを修正

--- spec/requests/user/comments_request_spec.rb
+++ spec/requests/user/comments_request_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "User::Comments", type: :request do
-
   describe "ユーザー/コメント新規登録ページ" do
     context "ユーザー/コメント新規登録ページが正しく表示される" do
-      let(:movie) { {id: 1, movie_image: "no_image.jpg", title: 'test1', summary: 'test1', a_movie_released: 'test1', show_time: 'test1'} }
-      let(:review) { {id: 1, movie_id: 1, user_id: 1, content: 'test1', is_spoil: true } }
       before do
-        @movie = Movie.create(movie)
-        @review = Review.create(review)
+        @user = FactoryBot.create(:user)
+        @movie = FactoryBot.create(:movie)
+        @review = FactoryBot.create(:review)
         get new_user_movie_review_comment_path(movie_id: @movie.id, review_id: @review.id)
       end
 
