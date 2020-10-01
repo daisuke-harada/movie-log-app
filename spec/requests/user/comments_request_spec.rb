7ccb3429f567b7e4d62c9f945d3aed2c81950780
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2020-10-01 17:33:29 +0900

    [fix] インデントとRspec

--- spec/requests/user/comments_request_spec.rb
+++ spec/requests/user/comments_request_spec.rb
@@ -1,21 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "User::Comments", type: :request do
-  describe "ユーザー/コメント新規登録ページ" do
+  describe "ユーザー/一覧" do
     context "ユーザー/コメント新規登録ページが正しく表示される" do
       before do
         @user = FactoryBot.create(:user)
         @movie = FactoryBot.create(:movie)
         @review = FactoryBot.create(:review)
-        get new_user_movie_review_comment_path(movie_id: @movie.id, review_id: @review.id)
+        get user_movie_review_comments_path(movie_id: @movie.id, review_id: @review.id)
       end
 
       it "リクエストは200okとなること" do
         expect(response.status).to eq 200
-      end
-
-      it "タイトルが正しく表示されていること" do
-        expect(response.body).to include("コメント投稿ページ")
       end
 
       it "フォームのタイトルが正しく表示されていること" do
